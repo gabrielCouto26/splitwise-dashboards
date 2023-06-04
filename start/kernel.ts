528d0201e4a1360f3b4b2caf00fff17035d25556
Gabriel Couto <gabrielcouto26@gmail.com>
2023-06-04 12:17:15 -0300

    configura adaptador inertia

--- start/kernel.ts
+++ start/kernel.ts
@@ -22,6 +22,9 @@ import Server from '@ioc:Adonis/Core/Server'
 */
 Server.middleware.register([
   () => import('@ioc:Adonis/Core/BodyParser'),
+
+  // Import the inertia-adonis middleware
+  () => import('@ioc:EidelLev/Inertia/Middleware'),
 ])
 
 /*
