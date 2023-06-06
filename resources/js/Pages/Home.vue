5ee2f85dc6005ad02f7065085e502895ad786a69
Gabriel Couto <gabrielcouto26@gmail.com>
2023-06-06 01:25:56 -0300

    primeira pagina teste

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  props: {
+    description: String
+  }
+}
+</script>
+
+<template>
+  <section>
+    <header>
+      <h1>Home</h1>
+      <p>{{ description }}</p>
+    </header>
+  </section>
+</template>
--- start/routes.ts
+++ start/routes.ts
@@ -20,8 +20,8 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async ({ view }) => {
-  return view.render('welcome')
+Route.get('/', async ({ inertia }) => {
+  return inertia.render('Home', { description: 'Hello World' })
 })
 
 Route.get('/splitwise', 'SplitwiseController.index')
