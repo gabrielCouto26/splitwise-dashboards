8e2de45f7838098055a7f2565fde2d806b5a2227
Gabriel Couto <gabrielcouto26@gmail.com>
2023-06-10 14:13:58 -0300

    implementa lista de grupos

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -9,10 +9,17 @@ export default {
 <template>
   <section>
     <header>
-      <h1>Home</h1>
-      <li v-for="group in groups">
-        {{ group }}
-      </li>
+      <h1 class="fs-1 px-4 py-4">Grupos ativos</h1>
+      <ol class="list-group list-group-numbered">
+        <li v-for="group in groups"
+          class="list-group-item list-group-item-action">
+          <a :href="`/groups/${group.id}`"
+            class="text-decoration-none"
+            style="color: inherit">
+            {{ group.name }}
+          </a>
+        </li>
+      </ol>
     </header>
   </section>
 </template>
