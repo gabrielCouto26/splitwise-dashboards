c4eb8b4c9667e3bac354df99f535ec806237fd6f
Gabriel Couto <gabrielcouto26@gmail.com>
2023-06-10 15:10:06 -0300

    inicia busca e visualização de despesas

--- app/Interfaces/Services/ISplitwiseService.ts
+++ app/Interfaces/Services/ISplitwiseService.ts
@@ -4,6 +4,6 @@ import { Group } from "../Group"
 export default interface ISplitwiseService {
   getGroups(): Promise<Group[]>
   getGroup(id: number): Promise<Group | null>
-  getExpenses(groupId: number): Promise<Expense[]>
-  getExpense(groupId: number, expenseId: number): Promise<Expense | null>
+  getExpenses(groupId?: number): Promise<Expense[]>
+  getExpense(expenseId: number): Promise<Expense | null>
 }
